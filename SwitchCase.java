246f983aec0c7203293cb30b2437a95b902c0937
being-krutik <66460732+being-krutik@users.noreply.github.com>
2020-07-10 18:03:20 +0530

    Create SwitchCase.java

---
+++ SwitchCase.java
@@ -0,0 +1,29 @@
+package mycaptain;
+
+import java.util.Scanner;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int choice;
+        System.out.println("Pick one: 1.Hi\t2. Hey\t3. Hello\t");
+        Scanner sc=new Scanner(System.in);
+        choice = sc.nextInt();
+        switch(choice)
+        {
+        case 1:
+        	System.out.println("You said Hi.");
+        	break;
+        case 2:
+        	System.out.println("You said Hey.");
+        	break;
+        case 3:
+        	System.out.println("You said Hello.");
+        	break;
+        default:
+        	System.out.println("You said nothing.");
+        }
+	}
+
+}
